bfddae44acd989cffa76124e4ef868aedbfa7e1e
junaga <hermann-stanew@invita.gmbh>
2024-02-18 18:35:55 +0000

    clean

--- build.sh
+++ build.sh
@@ -1,28 +1,29 @@
 #!/bin/bash
 set -e
 
-drive="./drive"
+dir="./dist/"
+rm -fr $dir
+mkdir -p $dir
 distro="http://deb.debian.org/debian/"
 release="stable"
 
 apt update
 apt install debootstrap extlinux fdisk -y
 
-rm -rf ./mnt/
-mkdir ./mnt/
+mkdir $dir/mnt/
 
-truncate -s 4G $drive
-printf "n\np\n1\n2048\n\na\nw\n" | fdisk $drive
+truncate -s 4G $dir/drive
+printf "n\np\n1\n2048\n\na\nw\n" | fdisk $dir/drive
 
 loopdev=$(losetup -f)
-losetup -o2048 $loopdev $drive
+losetup -o2048 $loopdev $dir/drive
 mkfs.ext4 $loopdev
-mount -t ext4 $loopdev ./mnt/
+mount -t ext4 $loopdev $dir/mnt/
 
-debootstrap $release ./mnt/ $distro
-cd ./mnt/
-mkdir -p ./boot/extlinux/
-extlinux -i ./boot/extlinux/
+debootstrap $release $dir/mnt/ $distro
+cd $dir/mnt/
+mkdir -p $dir/extlinux/
+extlinux -i $dir/extlinux/
 
 losetup -d $loopdev
 
