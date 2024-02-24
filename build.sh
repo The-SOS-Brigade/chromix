dc26b9a33133eae6597d08e978d617f2193df65a
xQuantx <xquantxz@gmail.com>
2024-02-24 01:21:35 +0000

    Chroot and setup the system
    Signed-off-by: xQuantx <xquantxz@gmail.com>

--- build.sh
+++ build.sh
@@ -15,15 +15,28 @@ truncate -s 4G $drive
 printf "n\np\n1\n2048\n\na\nw\n" | fdisk $drive
 
 loopdev=$(losetup -f)
-losetup -o2048 $loopdev $drive
+losetup -o1048576 $loopdev $drive
 mkfs.ext4 $loopdev
 mount -t ext4 $loopdev mnt
 
 debootstrap $release ./mnt $distro
-cd mnt
-mkdir -p boot/extlinux
-extlinux -i boot/extlinux
+mkdir -p ./mnt/boot/extlinux
+extlinux -i ./mnt/boot/extlinux
+cp extlinux.conf ./mnt/boot/extlinux
+
+mount --make-rslave --rbind /proc ./mnt/proc
+mount --make-rslave --rbind /sys ./mnt/sys
+mount --make-rslave --rbind /dev ./mnt/dev
+mount --make-rslave --rbind /run ./mnt/run
+cp chroot.sh ./mnt
+chmod +x ./mnt/chroot.sh
+chroot ./mnt "/chroot.sh"
+
+sync
 
 losetup -d $loopdev
+
+# haruhi mbr btw
+dd bs=440 count=1 conv=notrunc if=./mbr.bin of=$drive 
 
 echo System ready.
---
+++ chroot.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+export DEBIAN_FRONTEND=noninteractive DEBCONF_NONINTERACTIVE_SEEN=true
+
+useradd chromix -m -p ""
+usermod -aG sudo chromix
+printf "toor\ntoor\n" | passwd
+
+apt install ca-certificates -y
+
+cat > /etc/apt/sources.list << EOF
+deb https://deb.debian.org/debian/ bookworm main contrib non-free non-free-firmware
+deb-src https://deb.debian.org/debian/ bookworm main contrib non-free non-free-firmware
+
+deb https://security.debian.org/debian-security bookworm-security main contrib non-free non-free-firmware
+deb-src https://security.debian.org/debian-security bookworm-security main contrib non-free non-free-firmware
+
+deb https://deb.debian.org/debian/ bookworm-updates main contrib non-free non-free-firmware
+deb-src https://deb.debian.org/debian/ bookworm-updates main contrib non-free non-free-firmware
+EOF
+
+apt update
+apt install linux-image-6.1.0-18-amd64 network-manager console-setup console-setup-linux pciutils \
+    xserver-xorg-video-all xserver-xorg-input-evdev x11-xserver-utils \
+    x11-xkb-utils x11-utils xinit chromium -y
+
+cat > ~/.xinitrc << EOF
+#!/bin/bash
+exec chromium --no-sandbox
+EOF
+
+chmod +x ~/.xinitrc
+
+cat > /etc/resolv.conf << EOF
+nameserver 8.8.8.8
+nameserver 10.0.2.3
+
+EOF
+
+
