6d4bb239820388ebe9420af8743d0152fd2eec53
coderzawad <zawad@devsnest.llc>
2024-12-31 17:01:47 +0600

    Update chroot.sh
    Signed-off-by: coderzawad <zawad@devsnest.llc>

--- chroot.sh
+++ chroot.sh
@@ -46,7 +46,6 @@ tar -xvf /home/chromix/gotty_linux_amd64.tar.gz
 
 sudo cat > /etc/xdg/openbox/autostart << EOF
 bash /home/chromix/chrome-shutdown.sh
-bash /home/chromix/gotty --write-permit --port 9000 bash
 EOF
 
 cat > /home/chromix/chrome-shutdown.sh << EOF 
@@ -61,7 +60,8 @@ cat > /home/chromix/chrome-sleep.sh << EOF
 
 #!/bin/bash
 
-chromium --start-maximized
+chromium --start-maximized &
+/gotty --write-permit --port 9000 bash
 
 sleep 10
 
@@ -89,55 +89,56 @@ while true; do
 done
 
 EOF
-makedir -p /home/chromix/.config/chromium/Default/Bookmarks 
-cat > /home/chromix/.config/chromium/Default/Bookmarks << EOF
+# mkdir -p /home/chromix/.config/chromium/Default/
+# touch /home/chromix/.config/chromium/Default/Bookmarks
+# cat > /home/chromix/.config/chromium/Default/Bookmarks << EOF
 
-{
-   "checksum": "a99703a30470a0bbc90a88abe52547a2",
-   "roots": {
-      "bookmark_bar": {
-         "children": [ {
-            "date_added": "13380094980375747",
-            "date_last_used": "0",
-            "guid": "d237f4f0-f5ad-4e61-92b9-0fe5f4f608ae",
-            "id": "9",
-            "meta_info": {
-               "power_bookmark_meta": ""
-            },
-            "name": "Terminal",
-            "type": "url",
-            "url": "http://localhost:9000/"
-         } ],
-         "date_added": "13380041368295196",
-         "date_last_used": "0",
-         "date_modified": "13380095002652839",
-         "guid": "0bc5d13f-2cba-5d74-951f-3f233fe6c908",
-         "id": "1",
-         "name": "Bookmarks bar",
-         "type": "folder"
-      },
-      "other": {
-         "children": [  ],
-         "date_added": "13380041368295377",
-         "date_last_used": "0",
-         "date_modified": "13380094980375747",
-         "guid": "82b081ec-3dd3-529c-8475-ab6c344590dd",
-         "id": "2",
-         "name": "Other bookmarks",
-         "type": "folder"
-      },
-      "synced": {
-         "children": [  ],
-         "date_added": "13380041368295487",
-         "date_last_used": "0",
-         "date_modified": "0",
-         "guid": "4cf2e351-0e85-532b-bb37-df045d8f8d0f",
-         "id": "3",
-         "name": "Mobile bookmarks",
-         "type": "folder"
-      }
-   },
-   "version": 1
-}
+#{
+ #  "checksum": "a99703a30470a0bbc90a88abe52547a2",
+  # "roots": {
+   #   "bookmark_bar": {
+    #     "children": [ {
+      #      "date_added": "13380094980375747",
+       #     "date_last_used": "0",
+        #    "guid": "d237f4f0-f5ad-4e61-92b9-0fe5f4f608ae",
+         #   "id": "9",
+          #  "meta_info": {
+           #    "power_bookmark_meta": ""
+            #},
+            #"name": "Terminal",
+           # "type": "url",
+          #  "url": "http://localhost:9000/"
+       # # } ],
+      #   "date_added": "13380041368295196",
+     #    "date_last_used": "0",
+       #  "date_modified": "13380095002652839",
+      #   "guid": "0bc5d13f-2cba-5d74-951f-3f233fe6c908",
+        # "id": "1",
+         #"name": "Bookmarks bar",
+        # "type": "folder"
+      #},
+      #"other": {
+       #  "children": [  ],
+        # "date_added": "13380041368295377",
+         #"date_last_used": "0",
+         #"date_modified": "13380094980375747",
+         #"guid": "82b081ec-3dd3-529c-8475-ab6c344590dd",
+         #"id": "2",
+         #"name": "Other bookmarks",
+         #"type": "folder"
+      #},
+      #"synced": {
+       #  "children": [  ],
+        # "date_added": "13380041368295487",
+        # "date_last_used": "0",
+        # "date_modified": "0",
+        # "guid": "4cf2e351-0e85-532b-bb37-df045d8f8d0f",
+        # "id": "3",
+        # "name": "Mobile bookmarks",
+      #   "type": "folder"
+      #}
+  # },
+  # "version": 1
+#}
 
-EOF
+#EOF
